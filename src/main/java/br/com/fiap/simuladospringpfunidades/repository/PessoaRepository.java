9c79330873e3828fec11345893c350098ea2f649
ribeiromurilo <550858@fiap.com.br>
2024-04-23 20:18:40 -0300

    Ajustes do repository

--- src/main/java/br/com/fiap/simuladospringpfunidades/repository/PessoaRepository.java
+++ src/main/java/br/com/fiap/simuladospringpfunidades/repository/PessoaRepository.java
@@ -1,14 +1,12 @@
-//package br.com.fiap.simuladospringpfunidades.repository;
+package br.com.fiap.simuladospringpfunidades.repository;
 
-//import br.com.fiap.simuladospringpjunidades.entity.Pessoa;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
+import br.com.fiap.simuladospringpfunidades.entity.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//import java.util.List;
+import java.util.List;
 
-//@Repository
-//public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
- //   List<Pessoa> findByNomeContainsIgnoreCase(String nome);
-
-//}
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    List<Pessoa> findByNomeContainsIgnoreCase(String nome);
+}
--- src/main/java/br/com/fiap/simuladospringpfunidades/repository/UnidadeRepository.java
+++ src/main/java/br/com/fiap/simuladospringpfunidades/repository/UnidadeRepository.java
@@ -1,9 +1,9 @@
-//package br.com.fiap.simuladospringpfunidades.repository;
+package br.com.fiap.simuladospringpfunidades.repository;
 
-//import br.com.fiap.simuladospringpjunidades.entity.Unidade;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
+import br.com.fiap.simuladospringpfunidades.entity.Unidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface UnidadeRepository extends JpaRepository<Unidade, Long> {
-//}
+@Repository
+public interface UnidadeRepository extends JpaRepository<Unidade, Long> {
+}
